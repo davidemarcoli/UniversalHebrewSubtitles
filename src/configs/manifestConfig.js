be98e6ea025228ae841b7774b8feb861f979b6b9
Nitzan <nitzantomer1998@gmail.com>
2024-05-02 19:54:27 +0300

    Setting a manifestConfig Config

---
+++ src/configs/manifestConfig.js
@@ -0,0 +1,17 @@
+const manifestConfig = {
+  id: "wizdom.xyz",
+  version: "1.0.0",
+
+  catalogs: [],
+  resources: ["subtitles"],
+  types: ["movie", "series"],
+
+  name: "Wizdom Subtitles",
+  description: "An unofficial Stremio addon for Hebrew subtitles from wizdom.xyz",
+  logo: "https://i.ibb.co/KLYK0TH/wizdon256.png",
+
+  contactEmail: "Nitzantomer1998@gmail.com",
+};
+
+
+export default manifestConfig;
