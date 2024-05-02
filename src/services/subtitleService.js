cb4543becce57a271d75a4f7053209fea94ec3fa
Nitzan <nitzantomer1998@gmail.com>
2024-05-02 19:49:36 +0300

    Setting a subtitleService  Service

---
+++ src/services/subtitleService.js
@@ -0,0 +1,43 @@
+import JSZip from "jszip";
+import superagent from "superagent";
+
+import wizdomApi from "../apis/wizdomApi.js";
+import baseConfig from "../configs/baseConfig.js";
+import levenshtein from "../utils/levenshtein.js";
+
+
+const fetchSubtitlesFromWizdom = async (id, season = 0, episode = 0) => {
+  const url = `${wizdomApi.CONTENT_URL}/search?action=by_id&imdb=${id}&season=${season}&episode=${episode}`;
+  const response = await superagent.get(url);
+
+  return response.body;
+};
+
+const sortSubtitlesByFilename = (subtitles, filename) => {
+  return subtitles.sort((a, b) => {
+    const similarityA = levenshtein(a.versioname, filename);
+    const similarityB = levenshtein(b.versioname, filename);
+
+    return similarityA - similarityB;
+  });
+}
+
+const mapSubtitlesToStremioFormat = (subtitles) => {
+  return subtitles.map((s) => ({
+    url: `${baseConfig.BASE_URL}/${s.id}.srt`,
+    id: `${s.versioname}`,
+    lang: "heb",
+  }));
+};
+
+const downloadSubtitleZip = async (subtitleId) => {
+  const url = `${wizdomApi.DOWNLOAD_URL}/${subtitleId}`;
+  const response = await superagent.get(url).buffer(true);
+  const zip = await JSZip.loadAsync(response.body);
+  const srtContent = await zip.file(Object.keys(zip.files)[0]).async("string");
+
+  return srtContent;
+};
+
+
+export { fetchSubtitlesFromWizdom, sortSubtitlesByFilename, mapSubtitlesToStremioFormat, downloadSubtitleZip };
