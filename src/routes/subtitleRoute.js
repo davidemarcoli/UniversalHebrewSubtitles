b636d7fbcf10e94cb43ffd92f9e73ece94efd166
Nitzan <nitzantomer1998@gmail.com>
2024-05-07 18:00:07 +0300

    Updating Imports

--- src/routes/subtitleRoute.js
+++ src/routes/subtitleRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import corsMiddleware from "../middlewares/corsMiddleware.js";
-import { getManifest, getSubtitle, getSubtitles } from "../controllers/subtitleController.js";
+import { getManifest, getSubtitleSrt, getSubtitlesList } from "../controllers/subtitleController.js";
 
 
 const router = express.Router();
 router.get("/manifest.json", corsMiddleware, getManifest);
-router.get("/:subtitleId.srt", getSubtitle);
-router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, getSubtitles);
+router.get("/:subtitleId.srt", getSubtitleSrt);
+router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, getSubtitlesList);
 
 
 export default router;
