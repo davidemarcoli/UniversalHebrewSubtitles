77b169a7efd65d1e44ff120a7984025fff977956
Nitzan <nitzantomer1998@gmail.com>
2024-05-07 17:59:43 +0300

    Implementing Winston Logger

--- src/controllers/subtitleController.js
+++ src/controllers/subtitleController.js
@@ -1,3 +1,4 @@
+import logger from "../utils/logger.js";
 import manifestConfig from "../configs/manifestConfig.js";
 import extractFilename from "../utils/filenameExtractor.js";
 import extractCompoundID from "../utils/compoundIdExtractor.js";
@@ -5,31 +6,38 @@ import { fetchSubtitlesFromWizdom, sortSubtitlesByFilename, mapSubtitlesToStremi
 
 
 const getManifest = (req, res) => {
+  logger.info(`Manifest`);
+
   res.send(manifestConfig);
 };
 
-const getSubtitle = async (req, res) => {
+const getSubtitleSrt = async (req, res) => {
+  logger.info(`getSubtitleSrt(subtitleId=${req.params.subtitleId})`);
+
   const { subtitleId } = req.params;
   const srtContent = await extractSubtitleFromZipUrl(subtitleId);
 
   res.send(srtContent);
 };
 
-const getSubtitles = async (req, res) => {
+const getSubtitlesList = async (req, res) => {
   const { compoundID, extraArgs } = req.params;
+  const [id, season = 0, episode = 0] = extractCompoundID(compoundID);
   const filename = extractFilename(extraArgs);
 
+  logger.info(`getSubtitlesList(id=${id}, season=${season}, episode=${episode}, filename=${filename})`);
+
   try {
-    const wizdomSubtitles = await fetchSubtitlesFromWizdom(...extractCompoundID(compoundID));
+    const wizdomSubtitles = await fetchSubtitlesFromWizdom(id, season, episode);
     const sortedSubtitles = sortSubtitlesByFilename(wizdomSubtitles, filename);
     const stremioSubtitles = mapSubtitlesToStremioFormat(sortedSubtitles);
 
     res.send({ subtitles: stremioSubtitles });
   } catch (error) {
-    console.error(error);
+    logger.error(`Error: ${error}`);
     res.send({ subtitles: [] });
   }
 };
 
 
-export { getManifest, getSubtitle, getSubtitles };
+export { getManifest, getSubtitleSrt, getSubtitlesList };
