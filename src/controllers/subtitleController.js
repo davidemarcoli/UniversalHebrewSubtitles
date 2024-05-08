c23c35c81d7ef937d05e52440aa7847d127d4fec
Nitzan <nitzantomer1998@gmail.com>
2024-05-08 11:20:17 +0300

    Improving Logger

--- src/controllers/subtitleController.js
+++ src/controllers/subtitleController.js
@@ -6,13 +6,13 @@ import { fetchSubtitlesFromWizdom, sortSubtitlesByFilename, mapSubtitlesToStremi
 
 
 const getManifest = (req, res) => {
-  logger.info(`Manifest`);
+  logger.info(["Install", "Manifest Addon"]);
 
   res.send(manifestConfig);
 };
 
 const getSubtitleSrt = async (req, res) => {
-  logger.info(`getSubtitleSrt(subtitleId=${req.params.subtitleId})`);
+  logger.info(["Download", `subtitleId=${req.params.subtitleId}`]);
 
   const { subtitleId } = req.params;
   const srtContent = await extractSubtitleFromZipUrl(subtitleId);
@@ -25,16 +25,16 @@ const getSubtitlesList = async (req, res) => {
   const [id, season = 0, episode = 0] = extractCompoundID(compoundID);
   const filename = extractFilename(extraArgs);
 
-  logger.info(`getSubtitlesList(id=${id}, season=${season}, episode=${episode}, filename=${filename})`);
+  logger.info(["Watch", `id=${id}, season=${season}, episode=${episode}, filename=${filename}`]);
 
   try {
     const wizdomSubtitles = await fetchSubtitlesFromWizdom(id, season, episode);
-    const sortedSubtitles = sortSubtitlesByFilename(wizdomSubtitles, filename);
     const stremioSubtitles = mapSubtitlesToStremioFormat(sortedSubtitles);
+    const sortedSubtitles = sortSubtitlesByFilename(wizdomSubtitles, filename);
 
-    res.send({ subtitles: stremioSubtitles });
+    res.send({ subtitles: sortedSubtitles });
   } catch (error) {
-    logger.error(`Error: ${error}`);
+    logger.info(["Error", error]);
     res.send({ subtitles: [] });
   }
 };
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,11 +1,10 @@
-import winston from 'winston';
+import winston from "winston";
 
 
 const logger = winston.createLogger({
-    level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp({ format: 'DD/MM' }),
-        winston.format.printf(({ timestamp, level, message }) => `[${level}] ${timestamp}: ${message}`),
+        winston.format.timestamp({ format: "MM/DD HH:mm:ss" }),
+        winston.format.printf(({ timestamp, message }) => `[${message[0]}] >> ${message[1]} >> ${timestamp}`),
     ),
     transports: [new winston.transports.Console()],
 });
