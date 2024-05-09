dce917db5ccec0bcb0d59fb981a3220904eaea46
Nitzan <nitzantomer1998@gmail.com>
2024-05-09 16:31:18 +0300

    Updating Subtitle URL

--- src/services/subtitleService.js
+++ src/services/subtitleService.js
@@ -6,17 +6,23 @@ import baseConfig from "../configs/baseConfig.js";
 import levenshtein from "../utils/levenshtein.js";
 
 
-const fetchSubtitlesFromWizdom = async (id, season, episode) => {
-  const url = `${wizdomApi.CONTENT_URL}/search?action=by_id&imdb=${id}&season=${season}&episode=${episode}`;
+const fetchSubtitlesFromWizdom = async (imdbID, season, episode) => {
+  const url = `${wizdomApi.CONTENT_URL}/search?action=by_id&imdb=${imdbID}&season=${season}&episode=${episode}`;
   const response = await superagent.get(url);
   const wizdomSubtitles = response.body;
+
+  wizdomSubtitles.forEach((s) => {
+    s.imdbID = imdbID;
+    s.season = season;
+    s.episode = episode;
+  });
 
   return wizdomSubtitles;
 };
 
 const mapSubtitlesToStremioFormat = (subtitles) => {
   return subtitles.map((s) => ({
-    url: `${baseConfig.BASE_URL}/${s.id}.srt`,
+    url: `${baseConfig.BASE_URL}/${s.imdbID}/${s.season}/${s.episode}/${s.id}.srt`,
     id: `${s.versioname}`,
     lang: "heb",
   }));
@@ -31,8 +37,8 @@ const sortSubtitlesByFilename = (subtitles, filename) => {
   });
 }
 
-const extractSubtitleFromZipUrl = async (subtitleId) => {
-  const url = `${wizdomApi.DOWNLOAD_URL}/${subtitleId}`;
+const extractSubtitleFromZipUrl = async (subtitleID) => {
+  const url = `${wizdomApi.DOWNLOAD_URL}/${subtitleID}`;
   const response = await superagent.get(url).buffer(true);
   const zip = await JSZip.loadAsync(response.body);
   const srtContent = await zip.file(Object.keys(zip.files)[0]).async("string");
