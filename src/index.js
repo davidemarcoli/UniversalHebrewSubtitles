c615c60d3b300f6023b33a2d18bd8d7c6e196553
Nitzan <nitzantomer1998@gmail.com>
2024-05-09 16:52:02 +0300

    Removing MongoDB Configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
 
-import mongoDBConfig from "./configs/mongoDBConfig.js";
 import subtitleRoute from "./routes/subtitleRoute.js";
 import logger from "./utils/logger.js";
 
 
 dotenv.config();
-await mongoDBConfig();
 
 
 const app = express();
