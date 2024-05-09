688aee3c3123ec478300552ceb685f4f251f1284
Nitzan <nitzantomer1998@gmail.com>
2024-05-09 07:51:07 +0300

    Setting Time-Zone to Israel

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,11 @@
 import winston from "winston";
+import moment from "moment-timezone";
 
 
 const logger = winston.createLogger({
     format: winston.format.combine(
-        winston.format.timestamp({ format: "MM/DD HH:mm:ss" }),
-        winston.format.printf(({ timestamp, message }) => `[${message[0]}] >> ${message[1]} >> ${timestamp}`),
+        winston.format.timestamp({ format: () => moment().tz("Asia/Jerusalem").format("MM/DD HH:mm:ss"), }),
+        winston.format.printf(({ timestamp, message }) => `[${message[0]}] >> ${message[1]} << ${timestamp}`),
     ),
     transports: [new winston.transports.Console()],
 });
