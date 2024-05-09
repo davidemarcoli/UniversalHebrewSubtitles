5c166d27c5571688dd3601a08933afc4722ef548
Nitzan <nitzantomer1998@gmail.com>
2024-05-09 16:51:11 +0300

    Injecting TryCtach Wrapper

--- src/routes/subtitleRoute.js
+++ src/routes/subtitleRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 
+import wrapTryCatch from "../utils/wrapTryCatch.js";
 import corsMiddleware from "../middlewares/corsMiddleware.js";
 import { getManifest, getSubtitleSrt, getSubtitlesList } from "../controllers/subtitleController.js";
 
 
 const router = express.Router();
-router.get("/manifest.json", corsMiddleware, getManifest);
-router.get("/:imdbID/:season/:episode/:subtitleID.srt", getSubtitleSrt);
-router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, getSubtitlesList);
+router.get("/manifest.json", corsMiddleware, wrapTryCatch(getManifest));
+router.get("/:imdbID/:season/:episode/:subtitleID.srt", wrapTryCatch(getSubtitleSrt));
+router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, wrapTryCatch(getSubtitlesList));
 
 
 export default router;
