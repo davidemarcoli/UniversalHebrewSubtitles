297d02f3047590d3e2b2c00cfff2027dc1df92c7
Nitzan <nitzantomer1998@gmail.com>
2024-05-10 11:24:16 +0300

    Setting Validations

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -2,11 +2,12 @@ import db from "../configs/dbConfig.js";
 import addonInstallsQuery from "../queries/addonInstallsQuery.js";
 import downloadedContentQuery from "../queries/downloadedContentQuery.js";
 import watchedContentQuery from "../queries/watchedContentQuery.js";
+import dbValidation from "../validations/dbValidation.js";
 
 
-const insertAddonInstall = () => { db.query(addonInstallsQuery.insertInstall); };
-const insertDownloadedContent = (imdbID, season, episode) => { db.query(downloadedContentQuery.insertDownload, [imdbID, season, episode]); };
-const insertWatchedContent = (imdbID, season, episode, contentType) => { if (["series", "movie"].includes(contentType)) db.query(watchedContentQuery.insertWatch, [imdbID, season, episode]); };
+const insertAddonInstall = async () => { db.query(addonInstallsQuery.insertInstall); };
+const insertDownloadedContent = async (imdbID, season, episode) => { if (dbValidation.downloadedContent(imdbID, season, episode)) db.query(downloadedContentQuery.insertDownload, [imdbID, season, episode]); };
+const insertWatchedContent = async (contentType, imdbID, season, episode) => { if (dbValidation.watchedContent(contentType, imdbID, season, episode)) db.query(watchedContentQuery.insertWatch, [imdbID, season, episode]); };
 
 const dbService = {
     insertAddonInstall,
