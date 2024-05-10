c20b3357c2999ae21d11e6ef4d5491f88c28c756
Nitzan <nitzantomer1998@gmail.com>
2024-05-10 09:53:53 +0300

    Updating Logic

--- src/routes/subtitleRoute.js
+++ src/routes/subtitleRoute.js
@@ -1,14 +1,14 @@
 import express from "express";
 
-import wrapTryCatch from "../utils/wrapTryCatch.js";
+import subtitleController from "../controllers/subtitleController.js";
 import corsMiddleware from "../middlewares/corsMiddleware.js";
-import { getManifest, getSubtitleSrt, getSubtitlesList } from "../controllers/subtitleController.js";
+import wrapTryCatch from "../utils/wrapTryCatch.js";
 
 
 const router = express.Router();
-router.get("/manifest.json", corsMiddleware, wrapTryCatch(getManifest));
-router.get("/:imdbID/:season/:episode/:subtitleID.srt", wrapTryCatch(getSubtitleSrt));
-router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, wrapTryCatch(getSubtitlesList));
+router.get("/manifest.json", corsMiddleware, wrapTryCatch(subtitleController.getManifest));
+router.get("/:imdbID/:season/:episode/:subtitleID.srt", wrapTryCatch(subtitleController.getSubtitleSrt));
+router.get("/subtitles/:contentType/:compoundID/:extraArgs.json", corsMiddleware, wrapTryCatch(subtitleController.getSubtitlesList));
 
 
 export default router;
