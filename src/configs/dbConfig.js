c8d28e42303725990811828c441d9a6bbbe2a4f0
Nitzan <nitzantomer1998@gmail.com>
2024-05-12 09:28:40 +0300

    Setting Logger

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -1,9 +1,12 @@
 import pg from "pg";
 import dotenv from "dotenv";
+
+import loggerService from "../services/loggerService.js";
+
+
 dotenv.config();
-
-
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+loggerService.logDatabase();
 
 
 export default pool;
