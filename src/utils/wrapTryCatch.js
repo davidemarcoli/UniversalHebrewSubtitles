492d47c4f009d3fc04e4f55fda5dcd0e2761909e
Nitzan <nitzantomer1998@gmail.com>
2024-05-12 13:23:20 +0300

    Updating Error Log

--- src/utils/wrapTryCatch.js
+++ src/utils/wrapTryCatch.js
@@ -3,7 +3,7 @@ import loggerService from "../services/loggerService.js";
 
 const wrapTryCatch = (fn) => async (req, res) => {
     try { await fn(req, res); }
-    catch (error) { loggerService.logError(error); res.send({ subtitles: [] }); }
+    catch (error) { loggerService.logError(error.message); res.send({ subtitles: [] }); }
 };
 
 
