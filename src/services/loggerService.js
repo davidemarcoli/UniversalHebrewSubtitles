40d9f39d3831f74267c1535786bca9e8ac3b3be1
Nitzan <nitzantomer1998@gmail.com>
2024-05-12 09:31:35 +0300

    Setting Database Logger

--- src/services/loggerService.js
+++ src/services/loggerService.js
@@ -2,8 +2,9 @@ import logger from "../utils/logger.js";
 
 
 const logError = (error) => { logger.error(["Error", error]); };
-const logInstall = () => { logger.info(["Install", `Addon Installed`]); };
-const logExpress = () => { logger.info(["Express", `Express Connected`]); };
+const logInstall = () => { logger.info(["Install", "Addon Installed"]); };
+const logExpress = () => { logger.info(["Express", "Express Connected"]); };
+const logDatabase = () => { logger.info(["Database", "Database Connected"]); };
 const logDownload = (subtitleID) => { logger.info(["Download", `subtitleID=${subtitleID}`]); };
 const logWatch = (imdbID, season, episode) => { logger.info(["Watch", `imdbID=${imdbID}, season=${season}, episode=${episode}`]); };
 
@@ -11,6 +12,7 @@ const loggerService = {
     logError,
     logInstall,
     logExpress,
+    logDatabase,
     logDownload,
     logWatch,
 }
