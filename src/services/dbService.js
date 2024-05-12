fb87c2dca8813b60e81823a9aa9f2d16c856ce99
Nitzan <nitzantomer1998@gmail.com>
2024-05-12 09:31:12 +0300

    Removing Redundant Checks

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -2,12 +2,11 @@ import db from "../configs/dbConfig.js";
 import addonInstallsQuery from "../queries/addonInstallsQuery.js";
 import downloadedContentQuery from "../queries/downloadedContentQuery.js";
 import watchedContentQuery from "../queries/watchedContentQuery.js";
-import dbValidation from "../validations/dbValidation.js";
 
 
 const insertAddonInstall = async () => { db.query(addonInstallsQuery.insertInstall); };
-const insertDownloadedContent = async (imdbID, season, episode) => { if (dbValidation.downloadedContent(imdbID, season, episode)) db.query(downloadedContentQuery.insertDownload, [imdbID, season, episode]); };
-const insertWatchedContent = async (contentType, imdbID, season, episode) => { if (dbValidation.watchedContent(contentType, imdbID, season, episode)) db.query(watchedContentQuery.insertWatch, [imdbID, season, episode]); };
+const insertDownloadedContent = async (imdbID, season, episode) => { db.query(downloadedContentQuery.insertDownload, [imdbID, season, episode]); };
+const insertWatchedContent = async (imdbID, season, episode) => { db.query(watchedContentQuery.insertWatch, [imdbID, season, episode]); };
 
 const dbService = {
     insertAddonInstall,
