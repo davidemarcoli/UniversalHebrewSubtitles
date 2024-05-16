1b2dbf74ed25c4be944cb8b1af4b703ed235555f
Nitzan <nitzantomer1998@gmail.com>
2024-05-16 16:48:06 +0300

    Disable Developer Mode

--- src/controllers/stremioController.js
+++ src/controllers/stremioController.js
@@ -9,7 +9,7 @@ const getIcon = (req, res) => { res.sendFile("icon.svg", { root: "./public/icon"
 
 const getManifest = async (req, res) => {
   loggerService.logInstall();
-  // dbService.insertAddonInstall();
+  dbService.insertAddonInstall();
 
   res.send(manifestConfig);
 };
@@ -18,7 +18,7 @@ const getSubtitleSrt = async (req, res) => {
   const { source, imdbID, season, episode, subtitleID } = req.params;
 
   loggerService.logDownload(subtitleID);
-  // dbService.insertDownloadedContent(source, imdbID, season, episode);
+  dbService.insertDownloadedContent(source, imdbID, season, episode);
 
   const srtContent = await stremioService.getSubtitleSrt(source, subtitleID);
 
@@ -29,7 +29,7 @@ const getSubtitlesList = async (req, res) => {
   const { imdbID, season, episode, filename } = extractData(req.params);
 
   loggerService.logWatch(imdbID, season, episode);
-  // dbService.insertWatchedContent(imdbID, season, episode);
+  dbService.insertWatchedContent(imdbID, season, episode);
 
   const stremioSubtitles = await stremioService.getSubtitlesList(imdbID, season, episode);
   const sortedSubtitles = stremioService.sortSubtitlesByFilename(stremioSubtitles, filename);
