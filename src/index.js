2d6e86e0cad81c797d9df526961f57b8928bd7e2
Nitzan <nitzantomer1998@gmail.com>
2024-05-16 16:40:47 +0300

    Updating Imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 
 import baseConfig from "./configs/baseConfig.js";
-import subtitleRoute from "./routes/subtitleRoute.js";
+import stremioRoute from "./routes/stremioRoute.js";
 import loggerService from "./services/loggerService.js";
 
 
 const app = express();
-app.use("/", subtitleRoute);
+app.use("/", stremioRoute);
 app.listen(baseConfig.PORT, () => { loggerService.logExpress(); });
