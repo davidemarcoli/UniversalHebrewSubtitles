f299e1014b849e18fb8eae939e85349ddf37a189
Nitzan <nitzantomer1998@gmail.com>
2024-05-16 17:05:32 +0300

    Updating Logger Format

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,7 +4,7 @@ import moment from "moment-timezone";
 
 const logger = winston.createLogger({
     format: winston.format.combine(
-        winston.format.timestamp({ format: () => moment().tz("Asia/Jerusalem").format("MM/DD HH:mm:ss"), }),
+        winston.format.timestamp({ format: () => moment().tz("Asia/Jerusalem").format("DD/MM HH:mm:ss"), }),
         winston.format.printf(({ timestamp, message }) => `[${message[0]}] << ${message[1]} >> ${timestamp}`),
     ),
     transports: [new winston.transports.Console()],
